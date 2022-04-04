1fc296aa2b791b4265a80c0e79e26c98bbce2742
aadaml <74550173+aadaml@users.noreply.github.com>
2022-04-04 19:35:59 -0400

    Update Aadam.py

--- Aadam.py
+++ Aadam.py
@@ -1,1 +1,12 @@
+import pygame
 
+class Circle:
+    def __init__(self, x: int, y: int):
+        self.x = x
+        self.y = y
+        self.radius = 15
+        self.colour = (0, 0, 255)
+        self.width = 1
+
+    def draw(self, surface: pygame.Surface):
+        pygame.draw.circle(surface, self.colour, (self.x, self.y), self.radius, self.width)
