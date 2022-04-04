e7136b7aaca9fc9dd00589c74f084420a2d4a105
ivanchou4 <89237936+ivanchou4@users.noreply.github.com>
2022-04-04 14:17:45 -0400

    Create button.py

---
+++ button.py
@@ -0,0 +1,13 @@
+class ShapeCreatorButton:
+    def __init__(self, x: int, y: int, w: int, h: int):
+        print("Creating a button")
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+
+    def draw(self, surface: pygame.Surface): 
+        pygame.draw.rect(surface, (200, 200, 200), (self.x, self.y, self.width, self.height),0, 100)
+
+    def get_rect(self) -> pygame.Rect:
+        return pygame.Rect(self.x, self.y, self.width, self.height)
