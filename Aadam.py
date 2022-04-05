c3c75bef87b49e5ecfaad4dd466e2ce51b7619c2
aadaml <74550173+aadaml@users.noreply.github.com>
2022-04-05 12:58:20 -0400

    Update Aadam.py

--- Aadam.py
+++ Aadam.py
@@ -1,12 +1,74 @@
-import pygame
+# pygame template
+import random
 
-class Circle:
-    def __init__(self, x: int, y: int):
+import pygame
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+
+class Ball:
+    def __init__(self, x: int, y: int, dx: int, dy: int) -> None:
         self.x = x
         self.y = y
-        self.radius = 15
-        self.colour = (0, 0, 255)
-        self.width = 1
+        self.dx = dx
+        self.dy = dy
 
-    def draw(self, surface: pygame.Surface):
-        pygame.draw.circle(surface, self.colour, (self.x, self.y), self.radius, self.width)
+        r = random.randrange(0, 256,)
+        g = random.randrange(0, 256,)
+        b = random.randrange(0, 256,)
+        self.color = (r, g, b)
+
+pygame.init()
+
+WIDTH = 640
+HEIGHT = 480
+SIZE = (WIDTH, HEIGHT)
+
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+# ---------------------------
+# Initialize global variables
+
+balls = [
+    Ball(50, 50, 5, 1),
+    Ball(400, 200, -3, 1)
+]
+
+for _ in range(10):
+    x = random.randrange(0, WIDTH)
+    y = random.randrange(0, HEIGHT)
+    dx = random.randrange(-5, 5)
+    dy = random.randrange(-5, 5)
+    b = Ball(x, y, dx, dy)
+    balls.append(b)
+# ---------------------------
+
+running = True
+while running:
+    # EVENT HANDLING
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+
+    # GAME STATE UPDATES
+    # All game math and comparisons happen here
+    for ball in balls:
+        ball.x += ball.dx
+        ball.y += ball.dy
+
+    # DRAWING
+    screen.fill((255, 255, 255))  # always the first drawing command
+
+    for ball in balls:
+        pygame.draw.circle(screen, ball.color, (ball.x, ball.y), 30)
+
+    # Must be the last two lines
+    # of the game loop
+    pygame.display.flip()
+    clock.tick(30)
+    #---------------------------
+
+
+pygame.quit()
