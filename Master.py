11887b48318b27bca99d2b670d8c8635a224e072
Json-Lai <60758919+Json-Lai@users.noreply.github.com>
2022-04-06 13:58:50 -0400

    Update Master.py

--- Master.py
+++ Master.py
@@ -1,76 +1,44 @@
-
 import pygame
-from pygame.locals import K_ESCAPE, KEYDOWN, QUIT, MOUSEBUTTONDOWN
-from ball import Ball
-from button import ShapeCreatorButton
-pygame.init()
 
-WIDTH = 640
-HEIGHT = 480
-SIZE = (WIDTH, HEIGHT)
+class Player:
+    def __init__(self):
+        self.x= 100
+        self.y = 100
+        self.radius = 10
+        self.speed = 5
+        self.pressed_up = False
+        self.pressed_left = False
+        self.pressed_right = False
+        self.pressed_down = False
+        self.invulnerable = False
 
-screen = pygame.display.set_mode(SIZE)
-clock = pygame.time.Clock()
-
-# ---------------------------
-# Initialize global variables
-
-random_num = random.randint(1,1)
-balls = []
-button = ShapeCreatorButton(50, 50, 150, 50)
-for _ in range(2):
-    x = random.randrange(50, WIDTH-50)
-    y = random.randrange(50, HEIGHT-50)
-    dx = random.randrange(-5, 5)
-    dy = random.randrange(-5, 5)
-    radius = 40
-    b = Ball(x, y, dx, dy, radius)
-    balls.append(b)
-# ---------------------------
-
-running = True
-while running:
-    screen.fill((255, 255, 255))  # always the first drawing command
-    for ball in balls:
-        if ball.x + ball.radius > WIDTH or ball.x - ball.radius < 0:
-            ball.dx *= -1
-        if ball.y + ball.radius > HEIGHT or ball.y - ball.radius < 0:
-            ball.dy *= -1  
-        ball.x += ball.dx
-        ball.y += ball.dy
-    # EVENT HANDLING
-    for event in pygame.event.get():
-        if event.type == KEYDOWN:
-            if event.key == K_ESCAPE:
-                running = False
-        elif event.type == QUIT:
-            running = False
-        elif event.type == MOUSEBUTTONDOWN:
-            if button.get_rect().collidepoint(event.pos): 
-                balls.append(Ball(random.randrange(50, WIDTH-50), random.randrange(50, HEIGHT-50),random.randrange(-5, 5),random.randrange(-5, 5), 40))
-                print("Button CLicked")
-                
-
-    # GAME STATE UPDATES
-    # All game math and comparisons happen here
-
-
-            
-    # DRAWING
-    screen.fill((255, 255, 255))  # always the first drawing command
     
-    for ball in balls:
-        ball.draw(screen)
+    def move_up(self):
+        self.y -= self.speed
+    def move_down(self):
+        self.y += self.speed
+    def move_left(self):
+        self.x -= self.speed
+    def move_right(self):
+        self.x += self.speed
 
-    button.draw(screen)
+    def on_collision(self):
+        #on collision: reset game (remove items fomr balls and squares list, reset player position) ONLY IF PLAYER IS NOT INVULNERABLE
+        pass
+
+    def invulnerable(self):
+        #when you click the button, run this
+        self.invulnerable = True
+    
+    def draw(self, surface):        
+        pygame.draw.circle(surface, (0,0,10), (self.x,self.y), self.radius)
 
 
 
-    # Must be the last two lines
-    # of the game loop
-    pygame.display.flip()
-    clock.tick(30)
-    #---------------------------
 
 
-pygame.quit()
+# def dist_2_points(x1, y1, x2, y2):
+#     return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+# def detect_collision(distance, circle_r1, circle_r2):
+#     return distance < circle_r1 + circle_r2
