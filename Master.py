dbdefee6b2c2a4d4c725cfbcbb993c7eae5744b6
ivanchou4 <89237936+ivanchou4@users.noreply.github.com>
2022-04-06 13:55:50 -0400

    Update Master.py

--- Master.py
+++ Master.py
@@ -19,10 +19,29 @@ clock = pygame.time.Clock()
 # ---------------------------
 # Initialize global variables
 
+random_num = random.randint(1,1)
+balls = []
+button = ShapeCreatorButton(50, 50, 150, 50)
+for _ in range(2):
+    x = random.randrange(50, WIDTH-50)
+    y = random.randrange(50, HEIGHT-50)
+    dx = random.randrange(-5, 5)
+    dy = random.randrange(-5, 5)
+    radius = 40
+    b = Ball(x, y, dx, dy, radius)
+    balls.append(b)
 # ---------------------------
 
 running = True
 while running:
+    screen.fill((255, 255, 255))  # always the first drawing command
+    for ball in balls:
+        if ball.x + ball.radius > WIDTH or ball.x - ball.radius < 0:
+            ball.dx *= -1
+        if ball.y + ball.radius > HEIGHT or ball.y - ball.radius < 0:
+            ball.dy *= -1  
+        ball.x += ball.dx
+        ball.y += ball.dy
     # EVENT HANDLING
     for event in pygame.event.get():
         if event.type == KEYDOWN:
@@ -31,7 +50,10 @@ while running:
         elif event.type == QUIT:
             running = False
         elif event.type == MOUSEBUTTONDOWN:
-            pass
+            if button.get_rect().collidepoint(event.pos): 
+                balls.append(Ball(random.randrange(50, WIDTH-50), random.randrange(50, HEIGHT-50),random.randrange(-5, 5),random.randrange(-5, 5), 40))
+                print("Button CLicked")
+                
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
