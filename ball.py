6f833f1563d586c31c103edc7a56ed33c228702e
aadaml <74550173+aadaml@users.noreply.github.com>
2022-04-06 13:52:48 -0400

    Create ball.py

---
+++ ball.py
@@ -0,0 +1,14 @@
+class Ball:
+    def __init__(self, x: int, y: int, dx: int, dy: int, radius: int) -> None:
+        self.x = x
+        self.y = y
+        self.dx = dx
+        self.dy = dy
+        self.radius = radius
+        r = random.randrange(0, 256)
+        g = random.randrange(0, 256)
+        b = random.randrange(0, 256)
+        self.color = (r, g, b)
+
+    def draw(self, surface):
+        pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
