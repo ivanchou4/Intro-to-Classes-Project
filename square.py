f6249a4e263c1d8605d10326f011a69eb1adc4b9
augustinso <60696742+augustinso@users.noreply.github.com>
2022-04-06 14:06:38 -0400

    Update square.py

--- square.py
+++ square.py
@@ -1,14 +1,51 @@
-#TEMPLATE FOR SQUARE CLASS
-import pygame
+# pygame template
 
-class Square:
-    def __init__(self, x:int, y:int): 
-        self.x = x
-        self.y = y
-        self.size = 10
-        
-    def move(self) -> None:
-        pass
-        
-    def draw(self, surface) -> None:
-        pygame.draw.rect(surface, (0, 0, 200), (self.x, self.y, self.size, self.size))
+import pygame
+from pygame.locals import K_ESCAPE, KEYDOWN, QUIT
+from square import Square
+from random import randrange
+pygame.init()
+
+WIDTH = 640
+HEIGHT = 480
+SIZE = (WIDTH, HEIGHT)
+
+screen = pygame.display.set_mode(SIZE)
+clock = pygame.time.Clock()
+
+# ---------------------------
+# Initialize global variables
+x = 300
+y = 300
+square = Square(x,y,5,7)
+
+# ---------------------------
+
+running = True
+while running:
+    # EVENT HANDLING
+    for event in pygame.event.get():
+        if event.type == KEYDOWN:
+            if event.key == K_ESCAPE:
+                running = False
+        elif event.type == QUIT:
+            running = False
+
+    # GAME STATE UPDATES
+    # All game math and comparisons happen here
+
+    square.move()
+   
+
+    # DRAWING
+    screen.fill((255, 255, 255))  # always the first drawing command
+    square.draw(screen)
+    
+    # Must be the last two lines
+    # of the game loop
+    pygame.display.flip()
+    clock.tick(30)
+    #---------------------------
+
+
+pygame.quit()
