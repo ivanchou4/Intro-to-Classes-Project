0158088b92edb7f41f289c853fe86b2bad6a2388
augustinso <60696742+augustinso@users.noreply.github.com>
2022-04-06 14:12:58 -0400

    Update square.py

--- square.py
+++ square.py
@@ -1,14 +1,26 @@
-#TEMPLATE FOR SQUARE CLASS
 import pygame
+import math
+
 
 class Square:
-    def __init__(self, x:int, y:int): 
+
+    def __init__(self, x:int, y:int, speed_x: int, speed_y: int): 
         self.x = x
         self.y = y
         self.size = 10
-        
+        self.speed_x = speed_x
+        self.speed_y = speed_y
     def move(self) -> None:
-        pass
+        # self.x += self.speed_x
+        # self.y += self.speed_y
+        self.x += self.speed_x
+        self.y += 30*math.sin(self.y)
+        
+        if self.x < 0 or self.x > 640:
+            self.speed_x *= -1
+        if self.y <0 or self.y > 480:
+            self.speed_y *= -1
+        
         
     def draw(self, surface) -> None:
         pygame.draw.rect(surface, (0, 0, 200), (self.x, self.y, self.size, self.size))
