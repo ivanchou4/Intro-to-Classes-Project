dafa1438cc84f7b11f07d1ade068546226064788
aadaml <74550173+aadaml@users.noreply.github.com>
2022-04-07 11:30:13 -0400

    Update ball.py

--- ball.py
+++ ball.py
@@ -3,6 +3,18 @@ import pygame
 
 class Ball:
     def __init__(self, x: int, y: int, dx: int, dy: int, radius: int) -> None:
+        """Stores values for the ball/circle object
+        
+        Args:
+            x: x location
+            y: y location
+            dx: x speed
+            dy: y speed
+            radius: radius
+        
+        Returns:
+            None        
+        """
         self.x = x
         self.y = y
         self.dx = dx
@@ -13,5 +25,13 @@ class Ball:
         b = random.randrange(0, 256)
         self.color = (r, g, b)
 
-    def draw(self, surface):
+    def draw(self, surface: pygame.Surface) -> None:
+        """Allows the ball/circle object to be drawn with specific values  
+        
+        Args:
+            surface: pygame.Surface
+        
+        Returns:
+            None
+        """
         pygame.draw.circle(surface, self.color, (self.x, self.y), self.radius)
