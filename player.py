ebea5685332084f67e18c192ba5dbb3ee69e730d
Json-Lai <60758919+Json-Lai@users.noreply.github.com>
2022-04-07 16:35:59 -0400

    Update player.py

--- player.py
+++ player.py
@@ -1,8 +1,14 @@
 import pygame
 import math
 
+
 class Player:
     def __init__(self):
+        """Constructs attributes related to the class, runs when an instance of this class has been made
+
+        Args:
+            None
+        """
         self.x= 100
         self.y = 100
         self.radius = 10
@@ -13,20 +19,68 @@ class Player:
         self.pressed_down = False
         self.invulnerable = False
     
-    def move_up(self, width, height):
+    def move_up(self, width: int, height: int) -> None:
+        """Moves the player's circle up
+        
+        Args:
+            width: display window width
+            height: display window height
+
+        Returns:
+            None
+        """
         if self.y - self.radius >= 0:
             self.y -= self.speed
-    def move_down(self, width, height):
+            
+    def move_down(self, width: int, height: int) -> None:
+        """Moves the player's circle down
+        
+        Args:
+            width: display window width
+            height: display window height
+
+        Returns:
+            None
+        """
         if self.y + self.radius <= height:
             self.y += self.speed
-    def move_left(self, width, height):
+            
+    def move_left(self, width: int, height: int) -> None:
+        """Moves the player's circle left
+        
+        Args:
+            width: display window width
+            height: display window height
+
+        Returns:
+            None
+        """
         if self.x - self.radius >= 0:
             self.x -= self.speed
-    def move_right(self, width, height):
+            
+    def move_right(self, width: int, height: int) -> None:
+        """Moves the player's circle right
+        
+        Args:
+            width: display window width
+            height: display window height
+
+        Returns:
+            None
+        """
         if self.x + self.radius <= width:
             self.x += self.speed
 
-    def collision(self, squares, balls):
+    def collision(self, squares: list, balls: list) -> bool:
+        """Detects if the player's circle has a collision with the list of objects given in the parameters
+
+        Args:
+            squares: A list of squares
+            balls: A list of balls
+
+        Returns:
+            True if player's circle has a collision, False otherwise
+        """
         for ball in balls:
             distance = math.sqrt((ball.x - self.x)**2 + (ball.y - self.y)**2)
             if distance < ball.radius + self.radius:
@@ -36,6 +90,16 @@ class Player:
             distance = math.sqrt(((square.x + square_radius) - self.x)**2 + ((square.y + square_radius) - self.y)**2)
             if distance < square_radius + self.radius:
                 return True
-            
-    def draw(self, surface):        
+
+            return False
+                
+    def draw(self, surface: pygame.Surface) -> None:
+        """Draws the player's circle onto the screen
+
+        Args:
+            surface: A pygame surface
+
+        Returns:
+            None
+        """
         pygame.draw.circle(surface, (0,0,10), (self.x,self.y), self.radius)
