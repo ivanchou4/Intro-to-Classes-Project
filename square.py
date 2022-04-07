7ac6d8369f589957e6cefc975ed95b72ea452b95
augustinso <60696742+augustinso@users.noreply.github.com>
2022-04-07 11:44:39 -0400

    Update square.py

--- square.py
+++ square.py
@@ -8,13 +8,13 @@ class Square:
         """
         Takes all the attributes of the square. (x,y) position, size, and (x,y) velocity
         Args:
-        x: the x position of the square
-        y: the y position of the square
-        size: the length of the square's sides
-        speed_y: the speed of the square in the y direction
-        speed_x: the speed of the square in the x direction
+            x: the x position of the square
+            y: the y position of the square
+            size: the length of the square's sides
+            speed_y: the speed of the square in the y direction
+            speed_x: the speed of the square in the x direction
         Returns:
-        nothing
+            None
         
         """
         self.x = x
@@ -26,7 +26,9 @@ class Square:
         """
         updates the position of the squre for every tick to get the sq uare to move
         Args:
-        nothing
+            None
+        Returns:
+            None
         """
         self.x += self.speed_x
         self.y += self.speed_y
@@ -41,8 +43,8 @@ class Square:
         """
         Draws the square onto the screen
         Args: 
-        None
+            None
         Returns: 
-        None
+            None
         """
         pygame.draw.rect(surface, (0, 0, 200), (self.x, self.y, self.size, self.size))
