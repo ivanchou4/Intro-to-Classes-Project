e7fc2938657a51d702ce35866a97d390879148e7
ivanchou4 <89237936+ivanchou4@users.noreply.github.com>
2022-04-21 11:38:25 -0400

    Create play_button.py

---
+++ play_button.py
@@ -0,0 +1,43 @@
+import pygame
+
+class PlayButton:
+    def __init__(self, x: int, y: int, w: int, h: int, text: str, font):
+        """Initialize the button and text dimensions and location
+
+        Args:
+            x (int): x location of button
+            y (int): y location of button
+            w (int): width of button
+            h (int): height of button
+            text (str): text of the button
+            font (_type_): type of font used
+        
+        Returns:
+            None
+        """
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.text = font.render(text, False, (0,0,0))
+
+    def draw(self, surface: pygame.Surface) -> None:
+        """Draws the button and Text on the surface
+
+        Args:
+            surface (pygame.Surface): Screen where the button and text is drawn
+
+        Returns: 
+            None
+        """
+        pygame.draw.rect(surface, (0, 200, 0), (self.x, self.y, self.width, self.height),0, 100)
+        text_rect = self.text.get_rect(center=(self.x + self.width/2, self.y + self.height/2))
+        surface.blit(self.text, text_rect)
+
+    def get_rect(self) -> pygame.Rect:
+        """Returns Button Object dimensions and location
+
+        Returns:
+            pygame.Rect: dimensions and locations of the buttons
+        """
+        return pygame.Rect(self.x, self.y, self.width, self.height)
