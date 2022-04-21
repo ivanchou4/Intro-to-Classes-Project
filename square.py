f9196ff5ed8a4873168e34ead4dbc32697f1546a
Json-Lai <60758919+Json-Lai@users.noreply.github.com>
2022-04-21 18:02:58 -0400

    Update square.py

--- square.py
+++ square.py
@@ -1,50 +1,28 @@
 import pygame
-import math
-
 
 class Square:
-
-    def __init__(self, x:int, y:int, size: int, speed_x: int, speed_y: int): 
-        """
-        Takes all the attributes of the square. (x,y) position, size, and (x,y) velocity
-        Args:
-            x: the x position of the square
-            y: the y position of the square
-            size: the length of the square's sides
-            speed_y: the speed of the square in the y direction
-            speed_x: the speed of the square in the x direction
-        Returns:
-            None
-        
-        """
+    def __init__(self, x:int, y:int, speed_x: int, speed_y: int, size: int): 
         self.x = x
         self.y = y
-        self.size = 20
+        self.size = size
         self.speed_x = speed_x
         self.speed_y = speed_y
+        self.circle_x = x + self.size/2
+        self.circle_y = y + self.size/2
+        self.circle_radius = self.size/2
+    
     def move(self) -> None:
-        """
-        updates the position of the squre for every tick to get the sq uare to move
-        Args:
-            None
-        Returns:
-            None
-        """
         self.x += self.speed_x
         self.y += self.speed_y
         
         if self.x < 0 or self.x > 640:
             self.speed_x *= -1
-        if self.y <0 or self.y > 480:
+        if self.y < 0 or self.y > 480:
             self.speed_y *= -1
+
+        self.circle_x = self.x + self.size/2
+        self.circle_y = self.y + self.size/2
         
         
     def draw(self, surface) -> None:
-        """
-        Draws the square onto the screen
-        Args: 
-            None
-        Returns: 
-            None
-        """
         pygame.draw.rect(surface, (0, 0, 200), (self.x, self.y, self.size, self.size))
