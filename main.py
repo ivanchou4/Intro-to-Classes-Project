20cc73bf9531626918a7ab104ff8dba852c7b37f
ivanchou4 <89237936+ivanchou4@users.noreply.github.com>
2022-04-21 11:39:08 -0400

    Update main.py

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ from ball import Ball
 from button import ShapeCreatorButton
 from square import Square
 from player import Player
-from projectile import Projectile
+from projcetile import Projectile
+from pause_button import PauseButton
+from play_button import PlayButton
 
 pygame.init()
 
@@ -24,8 +26,10 @@ myfont = pygame.font.SysFont('Comic Sans MS', 30)
 
 balls = []
 squares = []
+play_button = PlayButton(500, 20, 100, 50, "Play", myfont)
+pause_button = PauseButton(320, 20, 100, 50, "Pause", myfont)  
 button = ShapeCreatorButton(20, 20, 150, 50, "ADD", myfont)
-
+pause = False
 pressed_up = False
 pressed_down = False
 pressed_left = False
@@ -65,61 +69,68 @@ while running:
         elif event.type == QUIT:
             running = False
         elif event.type == MOUSEBUTTONDOWN:
-            projectiles.append(Projectile(player.x, player.y, 2, event.pos[0], event.pos[1], 10))
+            if pause_button.get_rect().collidepoint(event.pos):
+                pause = True
+            if play_button.get_rect().collidepoint(event.pos):
+                pause = False
+
             if button.get_rect().collidepoint(event.pos): 
                 random_num = random.randint(1,2)
                 if random_num == 1:
                     balls.append(Ball(random.randrange(50, WIDTH-50), random.randrange(50, HEIGHT-50),random.randrange(-5, 5),random.randrange(-5, 5), 40))
                 if random_num == 2:
-                    squares.append(Square(random.randrange(50, WIDTH-50), random.randrange(50, HEIGHT-50), 5, 5))
+                    squares.append(Square(random.randrange(50, WIDTH-50), random.randrange(50, HEIGHT-50), 5, 5, 10))
                 
+            else:
+                projectiles.append(Projectile(player.x, player.y, 2, event.pos[0], event.pos[1], 10))
 
     # GAME STATE UPDATES
     # All game math and comparisons happen here
 
     #updates projectile positions and checks for its collisions
-    for projectile in projectiles:
-        projectile.move()
-        for square in squares:
-            if projectile.on_hit_square(square):
-                try:
-                    squares.remove(square)
-                    projectiles.remove(projectile)
-                except ValueError:
-                    pass
-        for ball in balls: 
-            if projectile.on_hit_ball(ball):
-                try:
-                    balls.remove(ball)
-                    projectiles.remove(projectile)
-                except ValueError:
-                    pass
-        
-    #updates square positions
-    for square in squares:
-        square.move()
+    if pause == False:
+        for projectile in projectiles:
+            projectile.move()
+            for square in squares:
+                if projectile.on_hit_square(square):
+                    try:
+                        squares.remove(square)
+                        projectiles.remove(projectile)
+                    except ValueError:
+                        pass
+            for ball in balls: 
+                if projectile.on_hit_ball(ball):
+                    try:
+                        balls.remove(ball)
+                        projectiles.remove(projectile)
+                    except ValueError:
+                        pass
             
-    #updates ball positions
-    for ball in balls:
-        ball.move(WIDTH, HEIGHT)
-        
-    #chceks for button clicks, if true, attempt to move the player
-    if pressed_up:
-        player.move_up(WIDTH, HEIGHT)
-    if pressed_down:
-        player.move_down(WIDTH, HEIGHT)
-    if pressed_left:
-        player.move_left(WIDTH, HEIGHT)
-    if pressed_right:
-        player.move_right(WIDTH, HEIGHT)
-        
-    #chceks for player collisions, if true, resets the game
-    if player.collision(squares, balls):
-        squares = []
-        balls = []
-        projectiles = []
-        player.x = 100
-        player.y = 100
+        #updates square positions
+        for square in squares:
+            square.move()
+                
+        #updates ball positions
+        for ball in balls:
+            ball.move(WIDTH, HEIGHT)
+            
+        #chceks for button clicks, if true, attempt to move the player
+        if pressed_up:
+            player.move_up(WIDTH, HEIGHT)
+        if pressed_down:
+            player.move_down(WIDTH, HEIGHT)
+        if pressed_left:
+            player.move_left(WIDTH, HEIGHT)
+        if pressed_right:
+            player.move_right(WIDTH, HEIGHT)
+            
+        #chceks for player collisions, if true, resets the game
+        if player.collision(squares, balls):
+            squares = []
+            balls = []
+            projectiles = []
+            player.x = 100
+            player.y = 100
         
     # DRAWING
     screen.fill((255, 255, 255))  # always the first drawing command
@@ -137,6 +148,10 @@ while running:
     for projectile in projectiles:
         projectile.draw(screen)
 
+    if len(balls) + len(squares) > 10: 
+        pause_button.draw(screen)
+        play_button.draw(screen)
+        
     # Must be the last two lines
     # of the game loop
     pygame.display.flip()
