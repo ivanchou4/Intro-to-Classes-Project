a6106850bcde395f104582013e8b9ed446e58472
Json-Lai <60758919+Json-Lai@users.noreply.github.com>
2022-04-29 12:50:08 -0400

    Update button.py

--- button.py
+++ button.py
@@ -3,7 +3,6 @@ import pygame
 class ShapeCreatorButton:
     def __init__(self, x: int, y: int, w: int, h: int, colour: tuple, text: str, font):
         """Initialize the button and text dimensions and location
-
         Args:
             x (int): x location of button
             y (int): y location of button
@@ -24,10 +23,8 @@ class ShapeCreatorButton:
 
     def draw(self, surface: pygame.Surface) -> None:
         """Draws the button and Text on the surface
-
         Args:
             surface (pygame.Surface): Screen where the button and text is drawn
-
         Returns: 
             None
         """
@@ -37,9 +34,7 @@ class ShapeCreatorButton:
 
     def get_rect(self) -> pygame.Rect:
         """Returns Button Object dimensions and location
-
         Returns:
             pygame.Rect: dimensions and locations of the buttons
         """
         return pygame.Rect(self.x, self.y, self.width, self.height)
-
