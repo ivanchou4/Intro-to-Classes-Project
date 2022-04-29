db1577896a66bc778e48eca80b8c4165750ecd94
Json-Lai <60758919+Json-Lai@users.noreply.github.com>
2022-04-29 12:56:42 -0400

    Update user_interface.py

--- user_interface.py
+++ user_interface.py
@@ -1,5 +1,4 @@
 import pygame
-from button import ShapeCreatorButton
 
 class UserInterface:
     def __init__(self, x: int, y: int, width: int, height: int, color: tuple, font, surface: pygame.surface):
@@ -27,7 +26,46 @@ class UserInterface:
         self.surface.blit(self.ui_bullets, (20, 425))
 
         #magazine
-        i = 0
+        self.offset = 0
         for num in range(0, magazine_bullets):
-            pygame.draw.circle(self.surface, (0, 0, 0), (110 + i, 435), 10)
-            i += 25
+            pygame.draw.circle(self.surface, (0, 0, 0), (110 + self.offset, 435), 10)
+            self.offset += 25
+
+class ShapeCreatorButton:
+    def __init__(self, x: int, y: int, w: int, h: int, colour: tuple, text: str, font):
+        """Initialize the button and text dimensions and location
+        Args:
+            x (int): x location of button
+            y (int): y location of button
+            w (int): width of button
+            h (int): height of button
+            text (str): text of the button
+            font (_type_): type of font used
+        
+        Returns:
+            None
+        """
+        self.x = x
+        self.y = y
+        self.width = w
+        self.height = h
+        self.colour = colour
+        self.text = font.render(text, False, (0,0,0))
+
+    def draw(self, surface: pygame.Surface) -> None:
+        """Draws the button and Text on the surface
+        Args:
+            surface (pygame.Surface): Screen where the button and text is drawn
+        Returns: 
+            None
+        """
+        pygame.draw.rect(surface, self.colour, (self.x, self.y, self.width, self.height),0, 100)
+        text_rect = self.text.get_rect(center=(self.x + self.width/2, self.y + self.height/2))
+        surface.blit(self.text, text_rect)
+
+    def get_rect(self) -> pygame.Rect:
+        """Returns Button Object dimensions and location
+        Returns:
+            pygame.Rect: dimensions and locations of the buttons
+        """
+        return pygame.Rect(self.x, self.y, self.width, self.height)
